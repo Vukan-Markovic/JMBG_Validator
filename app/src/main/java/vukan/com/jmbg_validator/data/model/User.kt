eac6d91084bcdedf494b3c6d6075fc8b06ce1cdb
Vukan-Markovic <vukan.markovic97@gmail.com>
2020-11-30 14:49:16 +0100

    final commit

--- app/src/main/java/vukan/com/jmbg_validator/data/model/User.kt
+++ app/src/main/java/vukan/com/jmbg_validator/data/model/User.kt
@@ -1,7 +1,9 @@
 package vukan.com.jmbg_validator.data.model
 
 import android.os.Parcelable
+import androidx.annotation.Keep
 import kotlinx.parcelize.Parcelize
 
+@Keep
 @Parcelize
 data class User(val name: String, val surname: String, val jmbg: String) : Parcelable
